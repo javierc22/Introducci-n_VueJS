0474d47bbc7d3cd4eca7bb62429ce826bfc3a2aa
javierc22 <javier.ec.22@gmail.com>
2019-02-14 16:28:59 -0300

    Tabla dinamica con Vue.js
    directiva v-for v-bind

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var app = new Vue({
-  el : '#app',
-  data : {
-    mensaje: 'Hola Vue.js!'
-  }
+    el : '#app',
+    data : {
+        lista : [
+            { nombre: 'Juan Carlos', promedio: '10', estado: false },
+            { nombre: 'Liliana Nataly', promedio: '17', estado: true },
+            { nombre: 'Luis Alberto', promedio: '18', estado: true }
+        ]
+    }
 })
