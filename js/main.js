b16c294f92078aed4073e23e01afe66ec5a43f9b
javierc22 <javier.ec.22@gmail.com>
2019-02-14 17:14:01 -0300

    Methods en Vue js
    Directivas v-on, v-if y v-else

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 var app = new Vue({
     el : '#app',
     data : {
-        lista : [
-            { nombre: 'Juan Carlos', promedio: '10', estado: false },
-            { nombre: 'Liliana Nataly', promedio: '17', estado: true },
-            { nombre: 'Luis Alberto', promedio: '18', estado: true }
-        ]
+        lista : [],
+        nombre : '',
+        promedio : ''
+    },
+
+    methods: {
+        agregarNota: function () {
+            estadoA = false;
+
+            if (this.promedio >= 12.5) {
+               estadoA = true;
+            }
+
+            if (this.nombre !="" && this.promedio !="") {
+                this.lista.push({ nombre: this.nombre, promedio: this.promedio, estado: estadoA });
+                this.nombre = "";
+                this.promedio = "";
+            } else {
+                alert("Ingrese el nombre y promedio del estudiante");
+            }
+        }
     }
 })
